7cd2bd025a33eb899e23d59bc3d6fcb14b476ce6
himanushi <himanushi@users.noreply.github.com>
2024-01-06 10:14:53 +0900

    fix

--- scripts/generateComment.js
+++ scripts/generateComment.js
@@ -7,7 +7,7 @@ const patterns = {
     filePath: (date) => `./public/comments/${date}.comment.md`,
     content: (date, content) => [
       {
-        content: `今日の日付は ${date} です。私のやる気とモチベーションを上げるように100文字程度でめちゃくちゃ褒め散らかしてください。フレンドリーな感じでお願いします。`,
+        content: `今日の日付は ${date} です。私のやる気とモチベーションを上げるように50文字程度でめちゃくちゃ褒め散らかしてください。フレンドリーな感じでお願いします。`,
         role: "system",
       },
       { content: content, role: "user" },
@@ -22,7 +22,7 @@ const patterns = {
         Math.floor(Math.random() * 2) === 0 ? "正位置" : "逆位置";
       return [
         {
-          content: `今日の日付は ${date} です。大アルカナだけでタロットを引きます。正位置、逆位置も必ず考慮してください。明日のタロット占いをしてください。100字程度でお願いします。`,
+          content: `今日の日付は ${date} です。大アルカナだけでタロットを引きます。正位置、逆位置も必ず考慮してください。明日のタロット占いをしてください。50字程度でお願いします。`,
           role: "system",
         },
         {
@@ -35,38 +35,40 @@ const patterns = {
 };
 
 const OPENAI_API_KEY = process.argv[2];
-const FILE_DATE = process.argv[3];
-const PATTERN = process.argv[4];
-const BLOG_FILE_PATH = `./public/blogs/${FILE_DATE}.md`;
+const PATTERN = process.argv[3];
 
 async function generateComment() {
-  const outputPath = patterns[PATTERN].filePath(FILE_DATE);
+  const dates = await readFile("./changed_dates.txt", "utf8");
+  for (const date of dates.split("\n")) {
+    if (date.trim() === "") continue;
 
-  try {
-    await access(outputPath, constants.F_OK);
-    if (patterns[PATTERN].once) {
-      return;
-    }
-  } catch (_error) {}
+    const outputPath = patterns[PATTERN].filePath(date);
 
-  const blogContent = await readFile(BLOG_FILE_PATH, "utf8");
+    try {
+      await access(outputPath, constants.F_OK);
+      if (patterns[PATTERN].once) {
+        return;
+      }
+    } catch (_error) {}
 
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      messages: patterns[PATTERN].content(FILE_DATE, blogContent),
-      model: "gpt-4",
-    }),
-  });
+    const blogContent = await readFile(`./public/blogs/${date}.md`, "utf8");
 
-  const data = await response.json();
-  const comment = data.choices[0].message.content;
-  console.log(comment);
-  await writeFile(patterns[PATTERN].filePath(FILE_DATE), comment, "utf8");
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        messages: patterns[PATTERN].content(date, blogContent),
+        model: "gpt-4",
+      }),
+    });
+
+    const data = await response.json();
+    const comment = data.choices[0].message.content;
+    await writeFile(patterns[PATTERN].filePath(date), comment, "utf8");
+  }
 }
 
 await generateComment();
