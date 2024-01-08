12532ea0586c93fad5d960bef50526859a930c17
himanushi <himanushi@users.noreply.github.com>
2024-01-08 20:19:15 +0900

    fix

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,9 +3,9 @@ import { blogList } from "~/store";
 export const Home = () => {
   return (
     <div>
-      <h1>Himanushi 雑記</h1>
+      <h1>Himanushi Lifelog</h1>
       <ul>
-        {blogList.value.map((post) => (
+        {blogList.value.reverse().map((post) => (
           <li key={post}>
             <a href={`/blogs/${post}`}>{post}</a>
           </li>
